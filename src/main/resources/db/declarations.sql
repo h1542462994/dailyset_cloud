8843ab2d6b59ad1a30d0f2393d490b6852f3b10f
h1542462994 <t1542462994@outlook.com>
2022-05-04 22:45:19 +0800

    Update declarations.sql

--- src/main/resources/db/declarations.sql
+++ src/main/resources/db/declarations.sql
@@ -53,3 +53,35 @@ CREATE TABLE IF NOT EXISTS `user_ticket_bind` (
     `ticket_id` VARCHAR(256) NOT NULL,
     PRIMARY KEY (uid)
 );
+
+# create table `dailyset` dsl
+CREATE TABLE IF NOT EXISTS `dailyset` (
+    `uid` VARCHAR(64) NOT NULL UNIQUE, # 唯一标识
+    `icon` VARCHAR(32) NOT NULL DEFAULT '', # 图标
+    `type` INTEGER NOT NULL, # 类型, normal = 0 | clazz = 1 | clazz_auto = 2 | task_specific = 3
+    `name` VARCHAR(64) NOT NULL,
+    `source_version` INTEGER NOT NULL, # 数据版本号
+    `matte_version` INTEGER NOT NULL, # 蒙版数据版本号
+    `meta_version` INTEGER NOT NULL # 元数据版本号
+);
+
+# create table `dailyset_usages` dsl
+CREATE TABLE IF NOT EXISTS `dailyset_usages` (
+    `user_uid` INTEGER NOT NULL, # 用户uid
+    `dailyset_uid` VARCHAR(64) NOT NULL, # 日程uid
+    `auth_type` INTEGER NOT NULL # 授权类型, owner = 0 | edit = 2 | read = 3
+);
+
+# create table `dailyset_source_item` dsl
+CREATE TABLE IF NOT EXISTS `dailyset_source_item` (
+    `uid` VARCHAR(64) NOT NULL,
+    `dailyset_uid` VARCHAR(64) NOT NULL,
+    `link_type` INTEGER NOT NULL, # 链接资源类型, course = 0 | duration = 1 | task = 2
+    `link_uid` VARCHAR(64) NOT NULL,
+    `insert_version` INTEGER NOT NULL, # 首次加入的版本
+    `remove_version` INTEGER NOT NULL, # 被移除的版本
+    `update_version` INTEGER NOT NULL # 更新的版本
+)
+
+
+
